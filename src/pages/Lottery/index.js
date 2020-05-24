1f0dc37b6ae6a2797fc1e8c5c9f7fa32cdb4c1ea
arthcp <arth@matic.network>
2020-05-24 19:34:46 +0530

    fetch and integrate lottery details

--- src/pages/Lottery/index.js
+++ src/pages/Lottery/index.js
@@ -1,52 +1,90 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CurrencyInputPanel from '../../components/CurrencyInputPanel'
 // import { useAddressBalance, useExchangeReserves } from '../../contexts/Balances'
-// import { useWeb3React, useExchangeContract } from '../../hooks'
+import { useWeb3React } from '../../hooks'
 // import { amountFormatter, calculateGasMargin } from '../../utils'
 import styled from 'styled-components'
 import { Button } from '../../theme'
 // import { ethers } from 'ethers'
+import { amountFormatter, calculateGasMargin, isAddress, getContract } from '../../utils'
 import * as constants from '../../constants'
+import CANDYSTORE_ABI from '../../constants/abis/candyStore.json'
+import CANDYARBER_ABI from '../../constants/abis/candyShopArber.json'
+import { BigNumber } from '@uniswap/sdk'
+import { ethers } from 'ethers'
 
+const Row = styled.div`
+  margin: 2rem 0 2rem 0;
+  display: flex;
+`
+
+const Col = styled.div`
+  width: 50%;
+  margin: 0 1rem 0 1rem;
+`
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 2rem;
+
+  button {
+    max-width: 20rem;
+  }
+`
+const TEN = new BigNumber(10)
+const DECIMAL_FACTOR = TEN.pow(18)
+
+let fetched = false
 export default function Lottery({ params }) {
-  // const { library, account, active, chainId } = useWeb3React()
-  const candiesOwned = 22
-  const candyPrice = 0.2
-  const drawPoolSize = 333
-  const committedPoolSize = 420
-  
-  const Row = styled.div`
-    margin: 2rem 0 2rem 0;
-    display: flex;
-  `
-  
-  const Col = styled.div`
-    width: 50%;
-    margin: 0 1rem 0 1rem;
-  `
+  const [candiesOwned, setCandiesOwned] = useState(22)
+  const [candyPrice, setCandyPrice] = useState(0)
+  const [drawPoolSize, setDrawPoolSize] = useState(333)
+  const [committedPoolSize, setCommittedPoolSize] = useState(420)
+  const [sponsorValue, setSponsorValue] = useState()
+  const [openDraw, setOpenDraw] = useState(0)
 
-  const Flex = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 2rem;
+  const inputCurrency = constants.ropstenDAI
+  const { library, account, active, chainId } = useWeb3React()
+  const candyStore = getContract(constants.CANDYSTORE_ADDRESS, CANDYSTORE_ABI, library)
+  const candyArber = getContract(constants.CANDYARBER_ADDRESS, CANDYARBER_ABI, library)
 
-    button {
-      max-width: 20rem;
+  useEffect(() => {
+    async function fetchOpenDraw() {
+      const newOpenDraw = await candyStore.openDraw()
+      if (newOpenDraw.toString(10) !== openDraw.toString(10)) {
+        setOpenDraw(newOpenDraw)
+      }
     }
-  `
-  
+    fetchOpenDraw()
+  })
+
+  useEffect(() => {
+    async function fetchDetails() {
+      const lottery = await candyStore.lottery(openDraw.toString(10))
+      const price = new BigNumber(lottery.candyPrice.toString())
+      setCandyPrice(price.div(DECIMAL_FACTOR))
+      const drawPoolSize = lottery.candyPrice.mul(lottery.totalCandy)
+      setDrawPoolSize(drawPoolSize)
+
+      const committedLottery = await candyStore.lottery((openDraw-1).toString(10))
+      const committedPoolSize = committedLottery.candyPrice.mul(committedLottery.totalCandy)
+      setCommittedPoolSize(committedPoolSize)
+    }
+    fetchDetails()
+  }, [openDraw])
+
+  // const inputBalance = useAddressBalance(account, inputCurrency)
+
   // function formatBalance(value) {
   //   return `Balance: ${value}`
   // }
 
-  const [sponsorValue, setSponsorValue] = useState()
-
-  const inputCurrency = constants.ropstenDAI
-  // const inputBalance = useAddressBalance(account, inputCurrency)
-
   function onSponsor() {
 
   }
+
+
 
   return (
     <div>
@@ -54,7 +92,7 @@ export default function Lottery({ params }) {
         <Col>
           <CurrencyInputPanel
             title="Candies Owned"
-            value={candiesOwned}
+            value={openDraw}
             disabled
             hideTokenSelect
           />
